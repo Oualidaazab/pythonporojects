960de54b3f81739ee6ab855adb9ca4a355eed32d
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2025-05-29 13:06:14 +0200

    Update exo7.py

--- exo7.py
+++ exo7.py
@@ -1,3 +1,19 @@
+#-------------------------------------------TASK-------------------------------------------------------|
+#Objectif : Appliquer le polymorphisme via des méthodes redéfinies.                                    |
+#Consignes :                                                                                           |
+#1. Créer une classe Animal avec une méthode parler() qui retourne "Je suis un                         |
+#animal".                                                                                              |
+#. Créer trois classes dérivées :                                                                      |
+#• Chien → "Wouf !"                                                                                    |
+# Chat → "Miaou !"                                                                                     |
+#• Oiseau → "Cui-cui !"                                                                                |
+#3. Créer une fonction faire_parler(animal) qui prend un objet Animal en paramètre                     |
+#et affiche le résultat de parler().                                                                   |
+#4. Tester cette fonction avec des objets des trois classes.                                           |
+#------------------------------------------------------------------------------------------------------|
+
+
+
 class Animal:
     def parler(self):
         pass
