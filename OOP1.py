08bebf3b9ae7510fa73996c5f877bc4f9bc00e94
oualid <aazabwalid20@gmail.com>
2025-06-03 17:40:05 +0200

    THE PYTHON PROJECT :.

---
+++ OOP1.py
@@ -0,0 +1,14 @@
+class  Etudiant: 
+    nombre_etudiants=0
+    def __init__(self,nom, prenom ,niveau ):
+         self.nom =nom 
+         self.prenom = prenom 
+         self.niveau =niveau 
+         Etudiant.nombre_etudiants+=1 
+    def afficher_infos(self) : 
+         return f" le nom est {self.nom} , est le prenom {self.prenom} est le niveau {self.niveau}" 
+Etudiant1=Etudiant("oualid","aazab","bac +1 ") 
+Etudiant2=Etudiant("ali ","ait bn ali ","bac") 
+print(Etudiant1.afficher_infos()) 
+print(Etudiant.nombre_etudiants) 
+ 
